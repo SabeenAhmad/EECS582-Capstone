6190d0fe771cfc5d60794ca8bab97418d41d3185
SabeenAhmad <106645636+SabeenAhmad@users.noreply.github.com>
2026-03-09 13:18:14 -0500

    fixing logos

--- app/app/_layout.js
+++ app/app/_layout.js
@@ -1,9 +1,19 @@
 // eecs/app/_layout.js
 import React from 'react';
 import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { Feather } from '@expo/vector-icons';
 import { ThemeProvider } from './context/ThemeContext';
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    ...Feather.font,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider>
       <Stack screenOptions={{ headerShown: false }} />
